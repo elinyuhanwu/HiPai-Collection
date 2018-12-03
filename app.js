b47365ce2ee65ef684b0eb5b86e1c64b912c2e59
Han <elinyuhanwu@gmail.com>
2018-12-03 16:26:12 +0000

    firstversion

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var app = express();
 app.set("view engine","ejs");
 app.use(express.static("public"));
 
+
 app.get("/",function(req, res){
     res.render("index");
 });
@@ -11,5 +12,25 @@ app.get("/",function(req, res){
 app.get("/tailoredsuits",function(req, res){
     res.render("former");
 });
+
+app.get("/students", function(req, res){
+   res.render("student"); 
+});
+
+app.get("/policeandmilitary", function(req, res){
+   res.render("policeMilitary"); 
+});
+
+app.get("/uniforms", function(req, res){
+    res.render("uniform");
+})
+
+app.get("/process", function(req, res){
+    res.render("flowchart");
+})
+
+app.get("/info", function(req, res){
+    res.render("map");
+})
 
 app.listen(process.env.PORT, process.env.IP);
